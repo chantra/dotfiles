b804480990f3f2362d11fad230eab3c163ead7d0
Manu Bretelle <chantr4@gmail.com>
2022-11-29 13:20:21 -0800

    [install] xdg_configs confs

--- install.sh
+++ install.sh
@@ -1,16 +1,43 @@
 #!/bin/bash
 
-for i in _*
-do
-  source="${PWD}/$i"
-  target="${HOME}/${i/_/.}"
+XDG_CONFIG_HOME=${XDG_CONFIG_HOME:-${HOME}/.config}
+
+set -x
+
+function link_file {
+  source="${1}"
+  target="${2}"
   if [ ! -L "${target}" ]
   then
     mv -v "${target}" "${target}.bak"
   else
-    rm -v ${target}
+    rm -v "${target}"
   fi
   ln -svf "${source}" "${target}"
+}
+
+
+for i in xdg_configs/*
+do
+  source_dir="${PWD}/$i"
+  target_dir="${XDG_CONFIG_HOME}/$(basename "${i}")"
+  if [[ ! -d "${target_dir}" ]]
+  then
+    mkdir "${target_dir}"
+  fi
+  for j in "${source_dir}"/*
+  do
+    source="${j}"
+    target="${XDG_CONFIG_HOME}/$(basename "${i}")/$(basename "${j}")"
+    link_file "${source}" "${target}"
+  done
+done
+
+for i in _*
+do
+  source="${PWD}/$i"
+  target="${HOME}/${i/_/.}"
+  link_file "${source}" "${target}"
 done
 
 git submodule sync
